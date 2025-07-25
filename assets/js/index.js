5030acdfbe7ec693e24c3dfbc37101c532b2a415
Alina Khametova <alinakhametova1@gmail.com>
2025-07-25 17:58:37 +0300

    add .

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
 //alert("Helo World");
-const arr = [1, 6, 4, 7];
-console.log("arr", arr);
+const numbers = [1, 6, 4, 7];
+
+console.log(numbers[0]);
+console.log(numbers[3]);
+
+delete numbers[1];
+delete numbers[3];
+console.log("numbers", numbers);
+
+console.log(numbers);
+console.log(numbers.length);
+
+const numbers2 = [...numbers];
