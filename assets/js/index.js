bf6fd64f86fa45ba8b4f78163c561ccd07671817
Alina Khametova <alinakhametova1@gmail.com>
2025-07-28 17:29:15 +0300

    add change

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,21 +3,12 @@ const numbers = [1, 6, 4, 7];
 
 numbers.push(28);
 console.log("numbers", numbers);
-numbers.pop(28);
+numbers.pop();
 console.log("numbers", numbers);
 numbers.unshift(37);
 console.log("numbers", numbers);
-numbers.shift(37);
+numbers.shift();
 console.log("numbers", numbers);
-
-/*const deletedNummbers = numbers.splice(3).concat(numbers.splice(0, 1));
-
-const deletedNummbers = [...numbers.splice(3), ...numbers.splice(0, 1)];
-
-*/
-
-console.log(numbers[0]);
-console.log(numbers[3]);
 
 console.log(numbers);
 console.log(numbers.length);
