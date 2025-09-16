daccad51ff93e2ca3da5694233d94f56e33fdbc5
Alina Khametova <alinakhametova1@gmail.com>
2025-09-16 20:42:57 +0300

    initial commit

---
+++ assets/js/classes.js
@@ -0,0 +1,17 @@
+class Post {
+  constructor(id, title, author, text, date) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.text = text;
+    this.date = date;
+  }
+}
+
+const post = new Post{
+    "id post",
+    "test",
+    "Testowich",
+    "this post about the test",
+    12.07.2023,
+};
