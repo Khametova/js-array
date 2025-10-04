be8ed3d3461cf15719356eb7cc652dc3d9daeede
Alina Khametova <alinakhametova1@gmail.com>
2025-10-04 20:00:12 +0300

    add 1,2 version

---
+++ assets/js/map.js
@@ -0,0 +1,10 @@
+const map1 = new Map();
+map1.set([1, "first"]), map1.set([3, "third"]);
+console.log("map1", map1);
+
+const map2 = new Map();
+map2.set([
+  [1, "first"],
+  [3, "third"],
+]);
+console.log("map2", map2);
