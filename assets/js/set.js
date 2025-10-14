c15edd2bf5271e248bfd662276c8239d9e16f03d
Alina Khametova <alinakhametova1@gmail.com>
2025-10-14 13:04:18 +0300

    add change

--- assets/js/set.js
+++ assets/js/set.js
@@ -5,14 +5,19 @@ console.log("s2.has(4)", s2.has(4));
 s2.add(4);
 console.log("s2.has(4)", s2.has(4));
 s2.add(5).add(23).add(100);
+s2.delete(2);
+console.log("s2", s2);
+s2.forEach((s) => console.log("s", s));
+for (const item of s2) console.log("item", item);
 
-//s2.forEach((s) => console.log("s", s));
-//const sum = s2.raduce((acc, val) => acc +)
-//console.log('sum', sum)
-for (const item of s2) {
-  console.log("item", item);
-}
+const arr4 = [...s2];
+console.log("arr4", arr4);
 
+const sum = arr4.reduce((acc, val) => acc + (val ?? 0), 0);
+
+console.log("sum", sum);
+s2.clear();
+console.log("s2", s2);
 const arr = [1, 6, 9, 4, 9, 1, 5, 6];
 const s3 = new Set(arr);
 const arr2 = [...new Set(arr)];
